74d62676d325d82d18dc085111491f937a1fa1e7
taaher-k <taaher.rehmanii@gmail.com>
2025-11-10 10:14:29 +0530

    add sub mul div
    completed

--- py4.1.2.py
+++ py4.1.2.py
@@ -3,3 +3,32 @@
     functions should read 2 values as input.      """
 
 
+def add(a,b):
+    c = a+b
+    print(c) 
+
+def sub(a,b):
+    c = a-b
+    print(c) 
+
+def mul(a,b):
+    c = a*b
+    print(c) 
+
+def div(a,b):
+    c = a/b
+    print(c) 
+
+
+
+first_num = int(input("enter the first number to calculate"))
+second_num = int(input("enter the second number to calculate"))
+
+
+
+
+add(first_num,second_num)
+sub(first_num,second_num)
+mul(first_num,second_num)
+div(first_num,second_num)
+
