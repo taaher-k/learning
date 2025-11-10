3106b253665cf9e058eae233658f446e31479db6
taaher-k <taaher.rehmanii@gmail.com>
2025-11-10 10:28:39 +0530

    add sub mul div
    correctly  completed

--- py4.1.2.py
+++ py4.1.2.py
@@ -1,7 +1,7 @@
 """ unit 4.1 task 2 Create functions to add(), subtract(), divide() and multiply(). All
     
     functions should read 2 values as input.      """
-
+"""
 
 def add(a,b):
     c = a+b
@@ -32,3 +32,37 @@ sub(first_num,second_num)
 mul(first_num,second_num)
 div(first_num,second_num)
 
+"""
+
+
+def add():
+    a = float(input("enter the first number to calculate"))
+    b = float(input("enter the second number to calculate"))
+    result = a+b
+    print(f"sum:{result}") 
+
+def sub():
+    a = float(input("enter the first number to calculate"))
+    b = float(input("enter the second number to calculate"))
+    result = a-b
+    print(f"sum:{result}")
+
+def mul(  ):
+    a = float(input("enter the first number to calculate"))
+    b = float(input("enter the second number to calculate"))
+    result = a*b
+    print(f"sum:{result}")
+
+def div():
+    a = float(input("enter the first number to calculate"))
+    b = float(input("enter the second number to calculate"))
+    if b != 0:
+       result = a/b
+       print(f"Quotient:{result}")
+    else:
+        print("Error: Division by zero is not allowed.")
+
+add()
+sub()
+mul()
+div()
